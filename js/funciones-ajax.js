2b563d496cb4b80833bbda4d515136ab97dc4bc7
freddy-mendez <freddy.mendez@misena.edu.co>
2023-10-05 10:39:04 -0500

    Ejercicio Completo

--- js/funciones-ajax.js
+++ js/funciones-ajax.js
@@ -29,6 +29,60 @@ function mostrar_informacion(data) {
     $(rs).append(tabla);
 }
 
+function  crear_tabla(genero, name, edad, celular, email, picture) {
+    var tabla = $("<table></table>");
+    var fila = $("<tr></tr>");
+    var celda = $("<td></td>");
+    var imagen = $("<img></img>");
+    $(imagen).attr("src",picture);
+    $(celda).append(imagen);
+    $(fila).append(celda);
+    celda = $("<td></td>");
+    $(celda).append(name);
+    $(fila).append(celda);
+    celda = $("<td></td>");
+    $(celda).append(edad);
+    $(fila).append(celda);
+    $(tabla).append(fila);
+    fila = $("<tr></tr>");
+    celda = $("<td></td>");
+    imagen = $("<img></img>");
+    if (genero=="male") {
+        $(imagen).attr("src","img/man.png");
+    } else if (genero=="female") {
+        $(imagen).attr("src","img/female.png");
+    }
+    $(imagen).attr("width","50px");
+    $(imagen).attr("height","50px");
+    $(celda).append(imagen);
+    $(fila).append(celda);
+    celda = $("<td></td>");
+    $(celda).attr("colspan","2");
+    $(celda).html("<p><a href='tel:+57"+celular+"'>"+celular+"</a><br/>"+
+    "<a href='mailto:"+email+"'>"+email+"</a></p>");
+    $(fila).append(celda);
+    $(tabla).append(fila);
+    return tabla;
+}
+
+function procesar_datos(data) {
+    let arreglo = data.results;
+    //console.log("Resultado="+arreglo.length+" objetos");
+    let rs = $("#result");
+
+    arreglo.forEach(element => {
+       //console.log(element);
+       var genero = element.gender;
+       var name = element.name.first+" "+element.name.last;
+       var edad = element.dob.age;
+       var celular = element.cell;
+       var email = element.email;
+       var picture = element.picture.large;
+       var tabla = crear_tabla(genero, name, edad, celular, email, picture);
+       $(rs).append(tabla);
+    });
+}
+
 $(document).ready(function () {
     $("#btnCarga").on("click", function () {
         console.log("OK");
@@ -45,10 +99,14 @@ $(document).ready(function () {
     });
     $("#btnCarga2").click(function () {
         $.ajax({
-            url: 'https://randomuser.me/api/',
+            url: 'json/users.json',
             dataType: 'json',
             success: function(data) {
               console.log(data);
+              procesar_datos(data);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
             }
           });
     });
